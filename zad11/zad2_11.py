ac335d6bb2fa5ac367a6aeaae43f7518dce596df
Marta <marta.jakacka@edu.uekat.pl>
2025-05-13 13:56:05 +0200

    add basic masking

---
+++ zad11/zad2_11.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+image=cv2.imread("osoba.jpg")
+
+mask = np.zeros(image.shape[:2], dtype="uint8")
+cv2.rectangle(mask, (200, 150), (600, 250), 255, -1)
+
+masked = cv2.bitwise_and(image, image, mask=mask)
+cv2.imshow("Mask Applied to Image", masked)
+cv2.waitKey()
+cv2.destroyAllWindows()
